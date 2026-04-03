7cbed87f6d3dbc625b1694ead560c75caf72e75c
methy <methymuy@gmail.com>
2026-04-03 22:25:17 +0700

    Start-code

---
+++ model/songs/song.dart
@@ -0,0 +1,32 @@
+class Song {
+  final String id;
+  final String title;
+  final String artistId;
+  final Duration duration;
+  final Uri imageUrl;
+  final int likes; 
+
+  Song({
+    required this.id,
+    required this.title,
+    required this.artistId,
+    required this.duration,
+    required this.imageUrl,
+    this.likes = 0, 
+  });
+
+  Song copyWith({int? likes}) {
+    return Song(
+      id: id,
+      title: title,
+      artistId: artistId,
+      duration: duration,
+      imageUrl: imageUrl,
+      likes: likes ?? this.likes,
+    );
+  }
+
+  @override
+  String toString() =>
+      'Song(id: $id, title: $title, artist id: $artistId, duration: $duration, likes: $likes)';
+}
